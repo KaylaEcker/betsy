da64dc10d3a48c819b543e546ae2a3cd8d55d7a4
Shaunna <skwiens@gmail.com>
2017-10-23 15:49:36 -0700

    order validations email and address for nonpending

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,10 @@ class Order < ApplicationRecord
   has_many :products, :through => :orderitems
   validates :status, presence: true
 
+  #checkout validations
+  validates :customer_email, presence: { :if => lambda { self.status != "pending"}, message: "customer email cannot be blank"}
+
+  validates :address1, presence: { :if => lambda { self.status != "pending"}, message: "address cannot be blank"}
+
+
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,15 +3,47 @@ require "test_helper"
 describe Order do
   let(:one) { orders(:one) }
 
-  it "must have one or more order items" do
-    one.orderitems.each do |o|
-      o.must_be_kind_of Orderitem
+  describe "validations" do
+    it "must have one or more order items" do
+      one.orderitems.each do |o|
+        o.must_be_kind_of Orderitem
+      end
+    end
+
+    it "can access products in the order" do
+      one.products.each do |o|
+        o.must_be_kind_of Product
+      end
     end
   end
 
-  it "can access products in the order" do
-    one.products.each do |o|
-      o.must_be_kind_of Product
+  describe "checkout validations" do
+    let(:pend_cust) {orders( :pending_customer) }
+
+    it "can have nil values if the status of the order is pending" do
+      order = Order.new(status: "pending")
+      order.valid?.must_equal true
     end
+
+    it "must have email if the status of the order is not pending" do
+      order = Order.new(status: "paid" )
+      order.valid?.must_equal false
+      order.errors.messages.must_include :customer_email
+      order.errors.messages[:customer_email].must_include "customer email cannot be blank"
+    end
+
+    it "must have an address if the status of the order is not pending" do
+      order = Order.new(status: "paid" )
+      order.valid?.must_equal false
+
+      order.errors.messages.must_include :address1
+      order.errors.messages[:address1].must_include "address cannot be blank"
+    end
+
+
+
+
+
   end
+
 end

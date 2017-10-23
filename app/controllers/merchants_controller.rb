772c4a9048121710d3c1d6c19e3bf43d70e8c749
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-23 15:10:34 -0700

    Merchants controller testing

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,26 +3,32 @@ class MerchantsController < ApplicationController
   end
 
   def fulfillment
-    @merchant = Merchant.find_by(id: params[:id])
-    status = params[:status]
-    orderitems = @merchant.join_orderitems(status)
-    @orders = {}
-    orderitems.each do |orderitem|
-      if @orders[orderitem.order_id.to_s] == nil
-        new_revenue = orderitem.quantity * orderitem.product.price
-        new_items = [orderitem]
-      else
-        new_revenue = @orders[orderitem.order_id.to_s][:revenue] + (orderitem.quantity * orderitem.product.price)
-        new_items = @orders[orderitem.order_id.to_s][:items]
-        new_items << orderitem
+    if params[:id] = session[:merchant_id]
+      @merchant = Merchant.find_by(id: params[:id])
+      status = params[:status]
+      orderitems = @merchant.join_orderitems(status)
+      @orders = {}
+      orderitems.each do |orderitem|
+        if @orders[orderitem.order_id.to_s] == nil
+          new_revenue = orderitem.quantity * orderitem.product.price
+          new_items = [orderitem]
+        else
+          new_revenue = @orders[orderitem.order_id.to_s][:revenue] + (orderitem.quantity * orderitem.product.price)
+          new_items = @orders[orderitem.order_id.to_s][:items]
+          new_items << orderitem
+        end
+        @orders[orderitem.order_id.to_s] = {status: orderitem.order.status, purchase_date: orderitem.order.purchase_datetime,revenue: new_revenue, items: new_items}
       end
-      @orders[orderitem.order_id.to_s] = {status: orderitem.order.status, purchase_date: orderitem.order.purchase_datetime,revenue: new_revenue, items: new_items}
-    end
-    @total_revenue = orderitems.sum {|orderitem| ( (orderitem.order.status == "cancelled") ? 0 : (orderitem.quantity * orderitem.product.price) ) }
-    @statuses = ["paid", "complete", "cancelled"]
-    @revenue_by_status = {}
-    @statuses.each do |a_status|
-      @revenue_by_status[a_status] = orderitems.sum {|orderitem| orderitem.order.status == a_status ? (orderitem.quantity * orderitem.product.price) : 0}
+      @total_revenue = orderitems.sum {|orderitem| ( (orderitem.order.status == "cancelled") ? 0 : (orderitem.quantity * orderitem.product.price) ) }
+      @statuses = ["paid", "complete", "cancelled"]
+      @revenue_by_status = {}
+      @statuses.each do |a_status|
+        @revenue_by_status[a_status] = orderitems.sum {|orderitem| orderitem.order.status == a_status ? (orderitem.quantity * orderitem.product.price) : 0}
+      end
+    else
+      flash[:status] = :error
+      flash[:result_text] = "Please log in first"
+      redirect_to root_path
     end
   end
 
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,34 +1,25 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "should get new" do
-  #   get merchants_new_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get create" do
-  #   get merchants_create_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get index" do
-  #   get merchants_index_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get edit" do
-  #   get merchants_edit_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get update" do
-  #   get merchants_update_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get show" do
-  #   get merchants_show_url
-  #   value(response).must_be :success?
-  # end
+  let(:merchant) {merchants(:sappy1)}
+  describe "#fulfillment" do
+    it "gets the fulfillment page with a valid merchant logged in" do
+      login(merchant, merchant.oauth_provider.to_sym)
+      session[:merchant_id].to_i.must_equal merchant.id
+      get merchant_fulfillment_path(merchant.id)
+      must_respond_with :success
+    end
 
+    it "redirects to the homepage when the merchant is not logged in" do
+      get merchant_fulfillment_path(merchant.id)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Please log in first"
+    end
+
+    it "redirects to the homepage when the merchant does not exist" do
+      get merchant_fulfillment_path(-1)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Please log in first"
+    end
+  end
 end

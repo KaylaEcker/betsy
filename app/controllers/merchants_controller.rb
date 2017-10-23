ce9cf75fea91f2e7696c3db40a89ab34af011f7e
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-23 13:34:13 -0700

    First pass at merchant fulfillment page: * Add merchant model method to get all relevant orderitems * Add merchants controller method to arrange order items into relevant orders

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,7 +3,23 @@ class MerchantsController < ApplicationController
   end
 
   def fulfillment
-
+    @merchant = Merchant.find_by(id: params[:id])
+    orderitems = @merchant.join_orderitems
+    @orders = {}
+    orderitems.each do |orderitem|
+      if @orders[orderitem.order_id.to_s] == nil
+        new_revenue = orderitem.quantity * orderitem.product.price
+        new_items = [orderitem]
+      else
+        new_revenue = @orders[orderitem.order_id.to_s][:revenue] + (orderitem.quantity * orderitem.product.price)
+        new_items = @orders[orderitem.order_id.to_s][:items]
+        new_items << orderitem
+      end
+      @orders[orderitem.order_id.to_s] = {status: orderitem.order.status, purchase_date: orderitem.order.purchase_datetime,revenue: new_revenue, items: new_items}
+      # @orders[orderitem.order_id.to_s][:revenue] += (orderitem.product.price * orderitem.quantity)
+    end
+    #all the unique order ids from @orderitems
+    #idea: make a hash with the order_id as the key and the revenue as the value
   end
 
   def create
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,7 +6,7 @@ class Merchant < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
 
-  def join_orderitems(merchant)
-    return Orderitem.joins('join products on orderitems.product_id = products.id').where("products.merchant_id = #{merchant.id}")
+  def join_orderitems
+    return Orderitem.joins('join products on orderitems.product_id = products.id').where("products.merchant_id = #{self.id}")
   end
 end

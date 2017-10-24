187acad5bd5a875fa7c1f258132e03482763b2b3
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-23 21:07:15 -0700

    Merchant controller testing

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe MerchantsController do
   let(:merchant) {merchants(:sappy1)}
   let(:other_merchant) {merchants(:sappy2)}
+  let(:merchant_order) { orders(:one) }
 
   describe "#fulfillment" do
     it "gets the fulfillment page with a valid merchant logged in" do
@@ -11,7 +12,16 @@ describe MerchantsController do
       get merchant_fulfillment_path(merchant.id)
       must_respond_with :success
     end
+
     it "redirects to the homepage when the merchant is not logged in" do
+      get merchant_fulfillment_path(merchant.id)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Unauthorized user"
+    end
+
+    it "redirects to the homepage when an invalid merchant is logged in" do
+      login(other_merchant, other_merchant.oauth_provider.to_sym)
+      session[:merchant_id].to_i.must_equal other_merchant.id
       get merchant_fulfillment_path(merchant.id)
       must_respond_with :redirect
       flash[:result_text].must_equal "Unauthorized user"
@@ -21,6 +31,42 @@ describe MerchantsController do
       get merchant_fulfillment_path(-1)
       must_respond_with :redirect
       flash[:result_text].must_equal "Invalid request"
+    end
+  end
+
+  describe "#show_order" do
+    it "gets the show order page with a valid merchant logged in" do
+      login(merchant, merchant.oauth_provider.to_sym)
+      session[:merchant_id].to_i.must_equal merchant.id
+      get merchant_order_path(merchant.id, merchant_order.id)
+      must_respond_with :success
+    end
+
+    it "redirects to the homepage when the merchant is not logged in" do
+      get merchant_order_path(merchant.id, merchant_order.id)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Unauthorized user"
+    end
+
+    it "redirects to the homepage when an invalid merchant is logged in" do
+      login(other_merchant, other_merchant.oauth_provider.to_sym)
+      session[:merchant_id].to_i.must_equal other_merchant.id
+      get merchant_order_path(merchant.id, merchant_order.id)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Unauthorized user"
+    end
+
+    it "redirects to the homepage when the merchant and order don't match" do
+      login(merchant, merchant.oauth_provider.to_sym)
+      session[:merchant_id].to_i.must_equal merchant.id
+
+      get merchant_order_path(-1, merchant_order.id)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Invalid request"
+
+      get merchant_order_path(merchant.id, -1)
+      must_respond_with :redirect
+      flash[:result_text].must_equal "Invalid order"
     end
   end
 

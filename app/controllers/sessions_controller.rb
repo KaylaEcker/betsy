e8a72637f11a6bb896e4eba5eb2f26aad39c4ffa
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-24 11:01:33 -0700

    Styles the flash notifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,13 @@ class SessionsController < ApplicationController
     if @merchant
       session[:merchant_id] = @merchant.id
       flash[:status] = :success
-      flash[:result_text] = "#{@merchant.username} is logged in!"
+      flash[:result_text] = "You successfully logged in as #{@merchant.username}."
     else
       @merchant = Merchant.new oauth_uid: @auth_hash['uid'], oauth_provider: @auth_hash['provider'], username: @auth_hash['info']['nickname'], email: @auth_hash['info']['email']
       if @merchant.save
         session[:merchant_id] = @merchant.id
         flash[:status] = :success
-        flash[:result_text] = "New user: #{@merchant.username} is logged in!"
+        flash[:result_text] = "You successfully logged in as new user #{@merchant.username}."
       else
         flash[:status] = :error
         flash[:result_text] = "Unable to save user :( "
